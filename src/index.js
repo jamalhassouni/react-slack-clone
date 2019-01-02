6e29f46b0e49e87a62dc17caa889b8c11bbcbca9
Jamal Hassouni <jamalhassouni13@gmail.com>
2019-01-02 17:21:03 +0100

    Add Spinner Component for Loading State

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import {
 import App from "./components/App";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
+import Spinner from "./Spinner";
 import "semantic-ui-css/semantic.min.css";
 import firebase from "firebase";
 import { createStore } from "redux";
@@ -29,7 +30,9 @@ class Root extends Component {
     });
   }
   render() {
-    return (
+    return this.props.isLoading ? (
+      <Spinner />
+    ) : (
       <Switch>
         <Route exact path="/" component={App} />
         <Route path="/login" component={Login} />
@@ -39,9 +42,13 @@ class Root extends Component {
   }
 }
 
+const mapStateFromProps = state => ({
+  isLoading: state.user.isLoading
+});
+
 const RootWithAuth = withRouter(
   connect(
-    null,
+    mapStateFromProps,
     { setUser }
   )(Root)
 );
