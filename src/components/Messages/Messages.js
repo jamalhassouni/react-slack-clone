e578ade574555be82f939d741f1d55e89b877f9a
Jamal Hassouni <jamalhassouni13@gmail.com>
2019-01-03 17:00:45 +0100

    Create Messages Collection,Add First Message to Database

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,17 +1,29 @@
 import React, { Component, Fragment } from "react";
+import firebase from "../../firebase";
 import { Segment, Comment } from "semantic-ui-react";
 import MessagesHeader from "./MessagesHeader";
 import MessageForm from "./MessageForm";
 
 export default class Messages extends Component {
+  state = {
+    messagesRef: firebase.database().ref("messages"),
+    channel: this.props.currentChannel,
+    user: this.props.currentUser
+  };
+
   render() {
+    const { messagesRef, channel, user } = this.state;
     return (
       <Fragment>
         <MessagesHeader />
         <Segment>
           <Comment.Group className="messages">{/* Messages */}</Comment.Group>
         </Segment>
-        <MessageForm />
+        <MessageForm
+          messagesRef={messagesRef}
+          currentChannel={channel}
+          currentUser={user}
+        />
       </Fragment>
     );
   }
