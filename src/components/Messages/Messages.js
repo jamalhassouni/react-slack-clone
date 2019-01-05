406b093afc5e533b80645f91ceb8d64fcf3bf5c7
Jamal Hassouni <jamalhassouni13@gmail.com>
2019-01-05 18:42:56 +0100

    Automatic Scroll to Bottom upon New Message

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -38,6 +38,16 @@ class Messages extends Component {
     }
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (this.messagesEnd) {
+      this.scrollToBottom();
+    }
+  }
+
+  scrollToBottom = () => {
+    this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+  };
+
   addListeners = channelId => {
     this.addMessageListener(channelId);
     this.addTypingListeners(channelId);
@@ -270,6 +280,7 @@ class Messages extends Component {
               ? this.displayMessages(searchResults)
               : this.displayMessages(messages)}
             {this.displayTypingUsers(typingUsers)}
+            <div ref={node => (this.messagesEnd = node)} />
           </Comment.Group>
         </Segment>
 
